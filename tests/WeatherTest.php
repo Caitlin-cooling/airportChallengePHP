91dc70bfa86df26f0194e05ddfe2ad5209baf02a
Caitlin Cooling <ccooling97@gmail.com>
2019-01-22 10:51:14 +0000

    Refactor to use classes file and only require once
    - This is to prevent the files being called too many times and causing 
    issues

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -1,6 +1,6 @@
 <?php
   declare(strict_types=1);
-  require __DIR__ . "/../src/Weather.php";
+  require_once('classes.php');
 
   use PHPUnit\Framework\TestCase;
 
---
+++ tests/classes.php
@@ -0,0 +1,5 @@
+<?php
+  require __DIR__ . "/../src/Airport.php";
+  require __DIR__ . "/../src/Plane.php";
+  require __DIR__ . "/../src/Weather.php";
+ ?>
